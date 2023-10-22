11e5466177d1ac947033ff120ebb17dbfe7309ea
Roman Urmancheev <matricarin@gmail.com>
2023-10-22 11:56:16 +0500

    dev crete viewmodel modelplugin and data

---
+++ Plugins/WPFFirstPlugin/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFFirstPlugin
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region implement property changed
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #endregion
+
+
+
+        #region Properies
+
+
+        #endregion
+
+    }
+}
---
+++ Plugins/WPFFirstPlugin/PluginData.cs
@@ -0,0 +1,21 @@
+﻿using Tekla.Structures.Plugins;
+
+namespace WPFFirstPlugin
+{
+    public class PluginData
+    {
+        //data in view
+        #region Fields
+
+        [StructuresField("name")]
+        public string partName;
+
+        [StructuresField("profile")]
+        public string partProfile;
+        
+        [StructuresField("material")]
+        public string material;
+
+        #endregion
+    }
+}
