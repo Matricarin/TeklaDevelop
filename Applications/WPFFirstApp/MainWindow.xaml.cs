338f5924288936455da68daa5ee91ff679130543
Roman Urmancheev <matricarin@gmail.com>
2023-10-24 13:19:25 +0500

    dev start view customize

--- Applications/WPFFirstApp/MainWindow.xaml.cs
+++ Applications/WPFFirstApp/MainWindow.xaml.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
+using Tekla.Structures.Dialog;
 
 namespace WPFFirstApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ApplicationWindowBase
     {
+        private MainWindowViewModel viewModel = new MainWindowViewModel();
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = viewModel;
+            this.InitializeDataStorage(viewModel);
+            if (this.GetConnectionStatus())
+            {
+                InitializeDistanceUnitDecimals();
+            }
         }
     }
 }
