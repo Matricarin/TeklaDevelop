a0e09550207e6338de4131ef0460ad5b306120b7
Roman Urmancheev <matricarin@gmail.com>
2023-10-24 13:19:05 +0500

    dev StartUpApp

--- Applications/WPFFirstApp/App.xaml.cs
+++ Applications/WPFFirstApp/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using TSM = Tekla.Structures.Model;
 
 namespace WPFFirstApp
 {
@@ -13,5 +8,26 @@ namespace WPFFirstApp
     /// </summary>
     public partial class App : Application
     {
+        private TSM.Events _events = new TSM.Events();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var window = new MainWindow();
+
+            if (new TSM.Model().GetConnectionStatus())
+            {
+                _events.TeklaStructuresExit += ExitEvent;
+
+                new System.Windows.Interop.WindowInteropHelper(window).Owner =
+                    Tekla.Structures.Dialog.MainWindow.Frame.Handle;
+                window.Show();
+            }
+        }
+
+        void ExitEvent()
+        {
+            base.Shutdown();
+        }
     }
 }
